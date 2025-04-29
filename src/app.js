33ae9c1f5316888b6d7129de8132e9e56369b687
shahid-jmi <shahidsalafi4@gmail.com>
2025-04-29 11:52:18 +0530

    Create a simple server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log("starting a new project");
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
